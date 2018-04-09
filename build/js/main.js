074c013bf7cfc2bcb8ff45fb5b033fa6ea599597
DevHosting <momotmiroslav@gmail.com>
2018-04-09 22:26:36 +0200

    work on project

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,41 @@
+var wWidth = $(window).width();
+const emailValidate = function () {
+	const reg = /^\w+@\w+\.[a-z]{2,}$/;
+	var text;
+	$('#email').blur(function () {
+		text = $(this).val();
+		if (!reg.test(text)) {
+            $(this).addClass('invalid');
+			$(this).next().addClass('active');
+			return;
+		}
+		$(this).removeClass('invalid');
+        $(this).next().removeClass('active');
+        if(!$('#email').hasClass('invalid') && !$('#name').hasClass('invalid')){
+            $('#submit').removeAttr('disabled')
+        }
+    })
+    
+};
+const nameValidate = function () {
+	const reg = /^[^ ]{3,20}$/;
+	var text;
+	$('#name').blur(function () {
+		text = $(this).val();
+		if (!reg.test(text)) {
+            $(this).addClass('invalid');    
+			$(this).next().addClass('active');
+			return;
+		}
+		$(this).removeClass('invalid');
+        $(this).next().removeClass('active');
+        if(!$('#email').hasClass('invalid') && !$('#name').hasClass('invalid')){
+            $('#submit').removeAttr('disabled')
+        }
+	})
+};
+
+$("#phone").mask("+380 (999) 999-9999");
 var mainSlider = function(selector){
     $(selector).slick({
         arrows: false,
@@ -29,6 +67,24 @@ $('.close_overlay').click(function(){
 $('.cancel_button').click(function(){
     $(this).parent().parent().removeClass('active');
 });
+
+if(wWidth < 1024){
+    $('.drop_trigger').click(function(e){
+        e.preventDefault();
+        $(this).next().toggleClass('open');
+    });
+    $('.burger').click(function(){
+        $(this).toggleClass('active');
+        $('.menu').toggleClass('active');
+        $('body').toggleClass('ovh');
+        $('.drop_trigger').next().removeClass('open');
+        $('.search_form').removeClass('active');
+        $('.search_trigger').removeClass('active');
+        $('.search_trigger .icon').removeClass('active');
+    });
+}
 $(document).ready(function(){
     mainSlider('.main_slider');
+    emailValidate();
+    nameValidate();
 });
